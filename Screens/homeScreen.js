10d38112709477fadd2ac1d605f34d2c28e82a07
lioswin <lioswinrite@gmail.com>
2024-02-27 11:01:21 +0300

    setup nativewind,reactNavigation

---
+++ Screens/homeScreen.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function HomeScreen() {
+    return (
+        <SafeAreaView>
+
+            <Text className="text-red-500">HomeSreen</Text>
+
+        </SafeAreaView>
+    );
+}
+
+
