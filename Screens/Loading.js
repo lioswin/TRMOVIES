4ee668905d1035e0700b2dd01bd0c5d2ebd5744d
lioswin <lioswinrite@gmail.com>
2024-02-28 10:18:58 +0300

    loading screen

--- Screens/Loading.js
+++ Screens/Loading.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Text } from 'react-native'
+import * as Progress from 'react-native-progress';
+import { Dimensions, Text } from 'react-native'
+import { theme } from '../theme';
 
+const {width,height} = Dimensions.get('window');
 export default function Loading() {
   return (
-    <View>
-        <Text>
-            Loading
-        </Text>
+    <View style={{height,width}} className="absolute flex-row justify-center items-center">
+       <Progress.CircleSnail thickness={12} size={160} color={theme.background}/>
     </View>
   )
 }
