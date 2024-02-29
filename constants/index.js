a441af264974923e9bb132cf8eebf310d74a2337
lioswin <lioswinrite@gmail.com>
2024-02-29 12:23:56 +0300

    dealing with api

---
+++ constants/index.js
@@ -0,0 +1,24 @@
+export const apiKey = 'bc0a009593c2d967ebfc80ab6952dec8'
+
+export const MoviesData = [
+    {
+        id: '',
+        title: '',
+        backgroundPoster: '',
+        image: '',
+        description: '',
+        releaseDate: '',
+        releaseYear: '',
+        runTime: ''
+    }
+]
+
+export const personData = {
+    name: '',
+    birthPlace: '',
+    birthDay: '',
+    gender: '',
+    KnownFor: '',
+    popularity:'',
+    biography:''
+}
