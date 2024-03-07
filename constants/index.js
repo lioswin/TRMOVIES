32a29d24b91d86d4e2f58b19a6670b962aa8dc10
lioswin <lioswinrite@gmail.com>
2024-03-07 15:24:36 +0300

    deleted keys exposed keys

--- constants/index.js
+++ constants/index.js
@@ -1,25 +1,2 @@
 export const apiKey = ''
 export const ApiAi = '';
-
-export const MoviesData = [
-    {
-        id: '',
-        title: '',
-        backgroundPoster: '',
-        image: '',
-        description: '',
-        releaseDate: '',
-        releaseYear: '',
-        runTime: ''
-    }
-]
-
-export const personData = {
-    name: '',
-    birthPlace: '',
-    birthDay: '',
-    gender: '',
-    KnownFor: '',
-    popularity:'',
-    biography:''
-}
